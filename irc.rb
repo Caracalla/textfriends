1469eac098010861375567acfb6f0c6397498ac9
Marcus Augustus <caracallarex@gmail.com>
2015-02-17 16:40:43 -0800

    Initial commit

---
+++ irc.rb
@@ -0,0 +1,27 @@
+require 'socket'
+
+network = "irc.foonetic.net"
+port = "6667"
+channel = "#rubytesting"
+nick = "rubybot_69"
+
+irc = TCPSocket.new(network, port)
+puts irc.recv(4096)
+irc.send("NICK #{nick}\n", 0)
+puts irc.recv(4096)
+irc.send("USER cool_dude * 0 :stranger danger\n", 0)
+
+while true
+	buffer = irc.recv(4096)
+	lines = buffer.split("\n")
+
+	lines.each do |line|
+		puts line
+		if line.include?("PING")
+			pong_out = line.split(":")[1]
+			irc.send("PONG :#{pong_out}",0)
+		end
+	end
+end
+
+irc.close
