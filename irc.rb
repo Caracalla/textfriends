def97747353bece4d20f85e5b8c4cd192bec44c5
James Anderson <caracallarex@gmail.com>
2016-01-28 10:56:07 -0800

    Fix spacing

--- irc.rb
+++ irc.rb
@@ -12,26 +12,26 @@ puts irc.recv(4096)
 irc.send("USER cool_dude * 0 :stranger danger\n", 0)
 
 connection = Thread.new {
-	while true
-		buffer = irc.recv(4096)
-		lines = buffer.split("\n")
+  while true
+    buffer = irc.recv(4096)
+    lines = buffer.split("\n")
 
-		lines.each do |line|
-			puts line
-			if line.include?("PING")
-				pong_out = line.split(":")[1]
-				irc.send("PONG :#{pong_out}",0)
-			end
-		end
-	end
+    lines.each do |line|
+      puts line
+      if line.include?("PING")
+        pong_out = line.split(":")[1]
+        irc.send("PONG :#{pong_out}",0)
+      end
+    end
+  end
 }
 
 input = Thread.new {
-	while true
-		user_input = gets
-		
-		irc.send(user_input, 0)
-	end
+  while true
+    user_input = gets
+
+    irc.send(user_input, 0)
+  end
 }
 
 connection.join
